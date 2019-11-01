b6a6617ffca4d521fdacd62bb49fd7bf8fe5409d
yashkotadia <yashkotadia@gmail.com>
2019-11-01 07:47:13 -0700

    .

--- train.py
+++ train.py
@@ -138,8 +138,8 @@ def train(train_loader, mapper, criterion, optimizer, epoch, log):
         if step % args.print_freq == 0:
             output = 'Epoch: [{0}][{1}/{2}]\t'.format(epoch + 1, step + 1, len(train_loader))
             output += 'lr: ' + str(optimizer.param_groups[-1]['lr']) + '\t'
-            output += 'Mean IOU: ' + str(mean_iou.avg.item()) + '\t' + 'Batch IOU: ' + str(iou) + '\t'
-            output += 'Mean Loss: ' + str(losses.avg.item()) + '\t' + 'Batch Loss: ' + str(loss.data[0]) +'\t'
+            output += 'Mean IOU: ' + str(mean_iou.avg.item()) + '\t' + 'Batch IOU: ' + str(iou.item()) + '\t'
+            output += 'Mean Loss: ' + str(losses.avg.item()) + '\t' + 'Batch Loss: ' + str(loss.data[0].item()) +'\t'
             output += 'Data Time: '+ str(data_time.avg) + '\t' + 'Batch Time: '+str(batch_time.avg)
             print(output)
             log.write(output + '\n')
@@ -173,8 +173,8 @@ def eval(val_loader, mapper, criterion, log, epoch):
         if step % args.print_freq == 0:
             output = 'Test: [{0}][{1}/{2}]\t'.format(epoch + 1, step + 1, len(val_loader))
             output += 'lr: ' + str(optimizer.param_groups[-1]['lr']) + '\t'
-            output += 'Mean IOU: ' + str(mean_iou.avg.item()) + '\t' + 'Batch IOU: ' + str(iou) + '\t'
-            output += 'Mean Loss: ' + str(losses.avg.item()) + '\t' + 'Batch Loss: ' + str(loss.data[0]) +'\t'
+            output += 'Mean IOU: ' + str(mean_iou.avg.item()) + '\t' + 'Batch IOU: ' + str(iou.item()) + '\t'
+            output += 'Mean Loss: ' + str(losses.avg.item()) + '\t' + 'Batch Loss: ' + str(loss.data[0].item()) +'\t'
             output += 'Data Time: '+ str(data_time.avg) + '\t' + 'Batch Time: '+str(batch_time.avg)
             print(output)
             log.write(output + '\n')
